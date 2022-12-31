7a742be16afd4780f3b5eb129bd731859e7a63d6
Sebastian <seba.binaghi96@gmail.com>
2022-12-31 18:27:48 -0300

    desafio 14

---
+++ desafio 14/Desafio-14-coder-back-main/faker.js
@@ -0,0 +1,19 @@
+
+// import {faker}from '@faker-js/faker';
+
+// const faker = require('faker');
+
+// const randomData = () => {
+// 	const productsFake = [];
+// 	for (let i = 0; i < 5; i++) {
+// 		const product = {
+// 			name: faker.name.firstName(),
+// 			price: faker.commerce.price(0, 500, 0, '$'),
+// 			img: faker.image.abstract()
+// 		}
+// 		productsFake.push(product);
+// 	}
+// 	return productsFake;
+// }
+
+// module.exports = randomData;
